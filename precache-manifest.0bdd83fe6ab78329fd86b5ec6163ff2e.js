d79863888453ddd8733a64399675a3b3745e712f
Андрій Данилко <example@gmail.com>
2020-04-15 12:14:04 +0300

    Updates

---
+++ precache-manifest.0bdd83fe6ab78329fd86b5ec6163ff2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c8162f9fd77bac74d9c49d1896911c",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "65e22efc1515cfca4962",
+    "url": "/react-test/static/css/main.82866a95.chunk.css"
+  },
+  {
+    "revision": "185da82f65b9081f2c60",
+    "url": "/react-test/static/js/2.74c848ee.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-test/static/js/2.74c848ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65e22efc1515cfca4962",
+    "url": "/react-test/static/js/main.b4f38fa4.chunk.js"
+  },
+  {
+    "revision": "0083664ca9b51b6ab068",
+    "url": "/react-test/static/js/runtime-main.eac4df24.js"
+  }
+]);
